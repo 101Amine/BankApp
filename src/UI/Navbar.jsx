ed81b314d36ca3b9600fe154f07f17fc3b8f1dc9
101Amine <aminebecha8@gmail.com>
2022-08-30 07:28:16 -0700

    header und the first item in the page

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -85,7 +85,7 @@ function Navbar() {
 
             <div className={`
               ${toggle ? "flex" :"hidden"} 
-              p-6 bg-slate-800	
+              p-6 bg-black-gradient	
               absolute top-20 right-0
               mx-4 my-2 min-w[140px] rounded-xl
               sidebar
